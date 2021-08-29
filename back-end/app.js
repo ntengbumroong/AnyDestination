e2662d7159a32ee03a10a139b847ee75bed1bf8f
Nathan Tengbumroong <ntengbumroong@gmail.com>
2021-08-28 22:00:14 -0700

    Reconfigured CORS

--- back-end/app.js
+++ back-end/app.js
@@ -10,9 +10,11 @@ const app= express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ['http://localhost:3000/', 'https://any-destination.netlify.app/']
-}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://any-destination.netlify.app/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
